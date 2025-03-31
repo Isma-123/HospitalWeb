cabd55a1d31649c0840e1fef5cd145bee466ffdd
Isma Segura <126912611+Isma-123@users.noreply.github.com>
2025-03-31 00:25:13 -0400

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HospitalsWeb.cs.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
